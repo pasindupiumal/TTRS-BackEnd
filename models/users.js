a8005637078e8b8e87dceb2be66377248323da24
unknown <pasindu0996@gmail.com>
2019-05-14 10:00:07 +0530

    Created Users Model

---
+++ models/users.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+    firstName: {
+        type:String,
+        required: true,
+        minLength: 2,
+        maxLength: 100
+    },
+    lastName: {
+        type:String,
+        required: true,
+        minLength: 2,
+        maxLength: 100
+    },
+    email:{
+        type:String,
+        required: true,
+        minLength: 5,
+        maxLength: 255,
+        unique: true
+    },
+    password:{
+        type:String,
+        required: true,
+        minLength: 5,
+        maxLength: 255,
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+function validateUser(user){
+
+    const userValidateSchema = {
+        firstName: Joi.string().min(2).max(100).required(),
+        lastName: Joi.string().min(2).max(100).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(user, userValidateSchema);
+}
+
+module.exports.User = User;
+module.exports.validateUser = validateUser;
